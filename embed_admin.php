4dc6ef9ee55f9656d93e04bbb06bb4395ad1cc12
gprialde <gprialde@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-11 20:16:38 +0000

    upload files
    git-svn-id: https://plugins.svn.wordpress.org/embed-articles/trunk@1005785 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ embed_admin.php
@@ -0,0 +1,14 @@
+<div class="wrap">
+	<?php    echo "<h2>" . __( 'Embed Articles Options', '' ) . "</h2>"; ?>
+	<?php if($updated==true) { echo "Your settings have been updated!"; }?>
+	<form name="emba_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+		<input type="hidden" name="emba_hidden" value="Y">
+		<?php    echo "<h4>" . __( 'Settings', '' ) . "</h4>"; ?>
+		<p><?php _e("API Key" ); ?><input type="text" name="pub_value" value="<?php echo $pub_value; ?>" size="30"></p>
+		<p>Get your own API Key Free at <a href="http://embedarticles.com/login/" target="_new">http://embedarticles.com/login/</a></p>
+		<?php    echo "<h4>" . __( 'Display Options', '' ) . "</h4>"; ?>
+		<p class="submit">
+		<input type="submit" name="Submit" value="<?php _e('Update Options', '' ) ?>" />
+		</p>
+	</form>
+</div>
