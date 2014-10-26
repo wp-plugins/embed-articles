9815f991d0b64c79ce9e698a62ac5fc88fa727c5
gprialde <gprialde@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-26 07:28:40 +0000

    deleting files
    git-svn-id: https://plugins.svn.wordpress.org/embed-articles/trunk@1014060 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tabs.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$("#tabs").tabs({
-		activate: function (event, ui) {
-			var active = $('#tabs').tabs('option', 'active');
-			$("#tabid").html('the tab id is ' + $("#tabs ul>li a").eq(active).attr("href"));
-
-		}
-	});
-});
