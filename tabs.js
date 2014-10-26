12e5a1ac2e79a2055d2074e6ee1d85a0bb2146e1
gprialde <gprialde@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-26 07:26:30 +0000

    uploading
    git-svn-id: https://plugins.svn.wordpress.org/embed-articles/trunk@1014059 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tabs.js
@@ -0,0 +1,9 @@
+$(function(){
+	$("#tabs").tabs({
+		activate: function (event, ui) {
+			var active = $('#tabs').tabs('option', 'active');
+			$("#tabid").html('the tab id is ' + $("#tabs ul>li a").eq(active).attr("href"));
+
+		}
+	});
+});
